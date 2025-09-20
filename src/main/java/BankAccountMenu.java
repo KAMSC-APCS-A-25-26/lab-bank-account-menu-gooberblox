73525024dd4154e791b75ea530ce50318ec5a4fa
gooberblox <bwrm2009@gmail.com>
2025-09-20 17:42:38 -0400

    gjflkfsjda;l

--- src/main/java/BankAccountMenu.java
+++ src/main/java/BankAccountMenu.java
@@ -9,5 +9,40 @@ public class BankAccountMenu {
         // 4. Use Scanner to read user input
         // 5. Use switch statement to handle menu choices
         // 6. Implement add money, withdraw money, check balance, and exit functionality
+
+        double balance = 0, amount;
+        int choice;
+        Scanner scan = new Scanner(System.in);
+
+        while(true) {
+
+            System.out.println("---Bank Account Menu---");
+            System.out.println("1. Add Money\n2. Withdrawal Money\n3. Check Balance\n4. Exit");
+            choice = scan.nextInt();
+
+            if (choice == 4) System.exit(0);
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter the amount you want to add: ");
+                    amount = scan.nextDouble();
+                    balance+=amount;
+                    System.out.println("Successfully added "+amount);
+                    System.out.println("New balance: "+balance);
+                    break;
+                case 2:
+                    System.out.println("Enter the amount you want to withdrawal");
+                    amount = scan.nextDouble();
+                    if(amount>balance) {
+                        System.out.println("Insufficient funds");
+                    } else {
+                        balance-=amount;
+                        System.out.println(amount+" withdrew.\nNew balance: "+balance);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Current balance: "+balance);
+            }
+        }
     }
 }
